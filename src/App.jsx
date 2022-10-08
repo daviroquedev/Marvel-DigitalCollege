7a9d76c97301e4ed7ddd85f2fb949ca7db4d98d8
Davi Roque <sowdavi@gmail.com>
2022-10-08 16:56:07 -0300

    routes add

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,13 @@
-import { useState } from "react";
-import { View } from "./components/View";
-import { CharacterGrid } from "./components/CharactedGrid";
-
-import { fetchApi } from "./api";
-import { useEffect } from "react";
+import {Routes, Route, BrowserRouter} from "react-router-dom"
+import {HomeViewContainer} from "./containers/views/HomeViewContainer"
 
 export const App = () => {
-  const [data, setData] = useState({});
-  const [loading, setLoading] = useState(false);
-  const [characters, setCharacters] = useState([]);
-
-  const fetchData = async () => {
-    try {
-      setLoading(true);
-      const response = await fetchApi("/characters");
-      const data = await response.json();
-      setData(data);
-    } catch (error) {
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  useEffect(() => {
-    if (data.data?.results) {
-      setCharacters(data.data.results);
-    }
-  }, [data]);
 
   return (
-    <View showLoader={loading}>
-      <CharacterGrid characters={characters} showLoading={loading} />
-    </View>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeViewContainer/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 };
---
+++ src/containers/views/HomeViewContainer.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { View } from "../../components/View";
+import { CharacterGrid } from "../../components/CharactedGrid";
+import { fetchApi } from "../../api";
+import { useEffect } from "react";
+
+export const HomeViewContainer = () => {
+  const [data, setData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [characters, setCharacters] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const response = await fetchApi("/characters");
+      const data = await response.json();
+      setData(data);
+    } catch (error) {
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (data.data?.results) {
+      setCharacters(data.data.results);
+    }
+  }, [data]);
+
+  return (
+    <View showLoader={loading}>
+      <CharacterGrid characters={characters} showLoading={loading} />
+    </View>
+  );
+};
